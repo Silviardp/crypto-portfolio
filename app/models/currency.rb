6f1e813f708f09883a3fb4dbad8b1cc374d0ae7d
Silvia Rocha <silviadprocha@gmail.com>
2020-05-25 17:34:23 +0900

    add current price crypto through api

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,19 @@
 class Currency < ApplicationRecord
+  def current_price
+     headers = {
+      "X-CMC_PRO_API_KEY" => "54496509-c9c0-415f-a178-6441129d31e3"
+    }
+    url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?slug=#{self.slug}"
+    request = HTTParty.get(url,
+      'Content-Type' => 'application/json',
+      :headers => headers
+    )
+    response = JSON.parse(request.body)
+    id = get_id(response.dig('data')).first
+    usd_price = response.dig('data', id, 'quote', 'USD', 'price')
+  end
+
+  def get_id(data)
+    id = data.keys
+  end
 end
