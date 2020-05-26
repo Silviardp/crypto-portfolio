8ad32c18f078178935ab54fa5c3a37ae8a5afb1d
Silvia Rocha <silviadprocha@gmail.com>
2020-05-26 15:31:09 +0900

    add form to search and handlechange method

--- app/javascript/components/Search.js
+++ app/javascript/components/Search.js
@@ -6,7 +6,26 @@ class Search extends Component {
   }
 
   render() {
-    return <div>Search goes here</div>;
+    return (
+      <div>
+        <h1>Cryptocurrency portfolio calculator</h1>
+        <form>
+          <div className="form-group">
+            <label>Search for a currency:</label>
+            <br />
+            <input
+              onChange={this.props.handleChange}
+              autoComplete="off"
+              type="text"
+              name="name"
+              placeholder="Ex: Bitcoin, Litecoin, Etherum, ..."
+              value={this.props.name}
+              className="field"
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
