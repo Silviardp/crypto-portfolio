8296730be5db442089ba01ec4a38ef9f923877fb
Silvia Rocha <silviadprocha@gmail.com>
2020-05-27 22:03:30 +0900

    add portfolio and portfolioitem components

---
+++ app/javascript/components/Portfolio.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class Portfolio extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="portfolio-value">
+          <div className="portfolio-value--header">
+            Your total portfolio value is:
+          </div>
+          <div className="portfolio-value--content">TOTAL</div>
+        </div>
+        <div className="portfolio-items"></div>
+      </div>
+    );
+  }
+}
+
+export default Portfolio;
